3fde1de897f64d1a51bb0b250331a4de223c014f
MohammadBohra <bohra.mohammadi@gmail.com>
2025-10-20 15:21:26 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '100px' }}>
-      <h1>🦷 Dentist Dashboard</h1>
-      <p>Hey there! Let’s manage some patient data 😎</p>
+      <h1>Dentist Dashboard</h1>
+      <p>Hey there Let’s manage some patient data</p>
     </div>
   );
 }
