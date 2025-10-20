d5dc05929e0f1083ec97c115693e6ccec96e27b6
MohammadBohra <bohra.mohammadi@gmail.com>
2025-10-20 15:40:46 +0530

    Update records.js

--- data/records.js
+++ data/records.js
@@ -1,1 +1,5 @@
-
+export const records = [
+  { id: 1, name: "John Doe", treatment: "Root Canal", age: 30 },
+  { id: 2, name: "Sarah Lee", treatment: "Teeth Whitening", age: 25 },
+  { id: 3, name: "Mark Patel", treatment: "Cavity Filling", age: 40 },
+];
